f983838a928524b7b7858bb8b46259dc55e9d24d
saikiran <thambasaikiran@gmail.com>
2023-05-25 04:30:06 +0000

    Init

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -8,31 +8,30 @@ class ReviewsCarousel extends Component {
 
   leftArrowFunction = () => {
     const {userName} = this.state
-    const list = this.itemFunction
+    const {reviewsList} = this.props
     if (userName !== 'Wade Warren') {
-      const index = list.findIndex(item => item.username === userName)
+      const index = reviewsList.findIndex(item => item.username === userName)
       const newIndex = index - 1
-      const newName = list[newIndex].username
+      const newName = reviewsList[newIndex].username
       this.setState({userName: newName})
     }
+    return null
   }
 
   rightArrowFunction = () => {
     const {userName} = this.state
-    const list = this.itemFunction
-    if (userName !== 'Infinos Tech') {
-      const index = list.findIndex(item => item.username === userName)
+    const {reviewsList} = this.props
+    if (userName !== 'Ronald Jones') {
+      const index = reviewsList.findIndex(item => item.username === userName)
       const newIndex = index + 1
-      const newName = list[newIndex].username
+      const newName = reviewsList[newIndex].username
       this.setState({userName: newName})
     }
+    return null
   }
-
-  itemFunction = reviewsList => reviewsList
 
   render() {
     const {reviewsList} = this.props
-    const itemFunc = this.itemFunction(reviewsList)
     const {userName} = this.state
     const item = reviewsList.filter(eachItem => eachItem.username === userName)
     const item1 = item[0]
@@ -41,22 +40,26 @@ class ReviewsCarousel extends Component {
         <div className="bg1">
           <h1>Reviews</h1>
           <div className="bg2">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
-              alt="left arrow"
-              onClick={this.leftArrowFunction}
-            />
+            <button type="button" data-testid="leftArrow">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+                alt="left arrow"
+                onClick={this.leftArrowFunction}
+              />
+            </button>
             <div className="bg3">
               <img src={item1.imgUrl} alt={item1.username} className="im" />
               <p>{item1.username}</p>
               <p className="p">{item1.companyName}</p>
               <p className="p">{item1.description}</p>
             </div>
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
-              alt="right arrow"
-              onClick={this.rightArrowFunction}
-            />
+            <button type="button" data-testid="rightArrow">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+                alt="right arrow"
+                onClick={this.rightArrowFunction}
+              />
+            </button>
           </div>
         </div>
       </div>
